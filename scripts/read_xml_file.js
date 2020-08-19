a29aef4f3010e498acd38f2933354438a73521e9
andrewdownsau <66402176+andrewdownsau@users.noreply.github.com>
2020-08-19 14:31:13 +1000

    Created read_xml_file.js

--- scripts/read_xml_file.js
+++ scripts/read_xml_file.js
@@ -1,5 +1,44 @@
 //Javascript for Terminal Command Cheatsheet WebApp
 //Function that reads the xml command data and builds 3 arrays that contain titles, descriptions and options
+   
+//Global Variable arrays for xml content
+var title = [];
+var description = [];
+var options = [];
 
+
+let getXMLFile = function(path, callback){
+	//Create a new request object
+	let request = new XMLHttpRequest();
+	
+	//Open the object and specify verb and file path 
+	request.open("GET", path);
+	
+	//Set the request header
+	request.setRequestHeader("Content-Type", "text/xml");
+	
+	//Ensure the XML file is set to the right format
+	request.overrideMimeType('application/xml');
+	
+	//Define the event listener
+	request.onreadystatechange = function() {
+		if (request.readyState == 4 && request.status == 200) {
+			callback(request.responseXML);
+		}
+	};
+	
+	//Send the request
+	request.send();
+};
+
+
+getXMLFile("raw_commands.xml", function(xml) {
+	//Log the document on the console to ensure there are no errors
+	console.log(xml);
+	
+	title = xmlDoc.getElementsByTagName("TITLE");
+	description = xmlDoc.getElementsByTagName("DESCRIPTION");
+	options = xmlDoc.getElementsByTagName("OPTIONS");
+});
 
 
